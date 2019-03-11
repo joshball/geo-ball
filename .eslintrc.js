fcc7e7508f6bc7eb436d6e430a731c3236534ea5
Joshua Ball <joshua.g.s.ball@gmail.com>
2019-03-11 16:16:09 -0600

    Cleaning up the perttier/eslint stuff.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,11 @@ module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     plugins: ['@typescript-eslint/tslint'],
     extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'prettier/react',
-        'plugin:react/recommended',
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        "prettier/react",
+        "plugin:react/recommended",
+        'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
     parserOptions: {
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -22,15 +22,12 @@ module.exports = {
         project: './tsconfig.common.json',
     },
     rules: {
-        //
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // https://eslint.org/docs/user-guide/configuring:
-        //  "off" or 0 - turn the rule off
-        //  "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
-        //  "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
-        //
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
         '@typescript-eslint/interface-name-prefix': 'always',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
