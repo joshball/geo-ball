63b28a00819aa8cb3ab3199f98822541ed9f8eb8
Joshua Ball <joshua.g.s.ball@gmail.com>
2019-03-24 08:40:19 -0600

    Adding in some more global types.

--- ext-stub-types/global.d.ts
+++ ext-stub-types/global.d.ts
@@ -1,1 +1,7 @@
 type Optional<T> = T | undefined;
+
+type VoidFunc = () => void;
+
+type SpreadFunc = (...args: Array<any>) => void;
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
